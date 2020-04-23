4c69120fdafcfd7f764d55377dd8cf0df1694168
Do Duc Huy <doduchuy@fit.cvut.com>
2020-04-24 01:42:01 +0200

    Added check for venv in setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,10 @@
 
 if [ ! -d __venv ]; then
 	python3 -m venv __venv
+	if [ $? -ne 0 ]; then
+		echo "Cannot create python virtual environment"
+		exit 1
+	fi
 fi
 source __venv/bin/activate
 pip install --upgrade pip
